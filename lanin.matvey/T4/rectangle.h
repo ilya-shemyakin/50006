75c77517705f9635c82cbcd29371eba562460f14
Lianueq <neverqlox@gmail.com>
2026-03-21 01:09:00 +0300

    Initial commit for T4: rectangle, ring, composite shape

---
+++ lanin.matvey/T4/rectangle.h
@@ -0,0 +1,22 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "Shape.h"
+
+class Rectangle : public Shape
+{
+private:
+  Point BottomLeft;
+  Point TopRight;
+
+public:
+  Rectangle(const Point& BottomLeft_, const Point& TopRight_);
+
+  double getArea() const override;
+  Point getCenter() const override;
+  void move(double dx, double dy) override;
+  void scale(double kef) override;
+  std::string getName() const override;
+};
+
+#endif
