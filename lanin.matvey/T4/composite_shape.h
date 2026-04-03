b88422cc36b841411c27c2d239aaa732a25f66fe
llll <88972901+Lianueq@users.noreply.github.com>
2026-04-03 14:53:13 +0300

    Fix composite_shape.h

--- lanin.matvey/T4/composite_shape.h
+++ lanin.matvey/T4/composite_shape.h
@@ -1,34 +1,34 @@
 #ifndef COMPOSITE_SHAPE_H
 #define COMPOSITE_SHAPE_H
 
-#include "Shape.h"
+#include "shape.h"
 #include <vector>
 #include <memory>
 
 class CompositeShape : public Shape
 {
 private:
-  std::vector<std::unique_ptr<Shape>> shapes;
+    std::vector<std::unique_ptr<Shape>> shapes;
 
 public:
-  CompositeShape() {}
+    CompositeShape() = default;
 
-  CompositeShape(const CompositeShape &) = delete;
-  CompositeShape &operator=(const CompositeShape &) = delete;
+    CompositeShape(const CompositeShape&) = delete;
+    CompositeShape& operator=(const CompositeShape&) = delete;
 
-  CompositeShape(CompositeShape &&) = default;
-  CompositeShape& operator=(CompositeShape&&) = default;
+    CompositeShape(CompositeShape&&) = default;
+    CompositeShape& operator=(CompositeShape&&) = default;
 
-  void addShape(std::unique_ptr<Shape> shape);
+    void addShape(std::unique_ptr<Shape> shape);
 
-  double getArea() const override;
-  Point getCenter() const override;
-  void move(double dx, double dy) override;
-  void scale(double kef) override;
-  std::string getName() const override;
+    double getArea() const override;
+    Point getCenter() const override;
+    void move(double dx, double dy) override;
+    void scale(double factor) override;
+    std::string getName() const override;
 
-  size_t getSize() const;
-  const std::vector<std::unique_ptr<Shape>>& getShapes() const;
+    size_t getSize() const;
+    const std::vector<std::unique_ptr<Shape>>& getShapes() const;
 };
 
 #endif
