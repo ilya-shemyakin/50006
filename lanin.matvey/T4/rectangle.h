b4a3d6cb888cb4455531c316b2fc2824181eb709
llll <88972901+Lianueq@users.noreply.github.com>
2026-04-03 14:51:32 +0300

    Fix rectangle.h

--- lanin.matvey/T4/rectangle.h
+++ lanin.matvey/T4/rectangle.h
@@ -1,22 +1,22 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
-#include "Shape.h"
+#include "shape.h"
 
 class Rectangle : public Shape
 {
 private:
-  Point BottomLeft;
-  Point TopRight;
+    Point bottomLeft;
+    Point topRight;
 
 public:
-  Rectangle(const Point& BottomLeft_, const Point& TopRight_);
+    Rectangle(const Point& bottomLeft_, const Point& topRight_);
 
-  double getArea() const override;
-  Point getCenter() const override;
-  void move(double dx, double dy) override;
-  void scale(double kef) override;
-  std::string getName() const override;
+    double getArea() const override;
+    Point getCenter() const override;
+    void move(double dx, double dy) override;
+    void scale(double factor) override;
+    std::string getName() const override;
 };
 
 #endif
