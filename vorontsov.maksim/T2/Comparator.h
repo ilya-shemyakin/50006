ccc3246d80fc592fa3abd29bcfd2ab7adf5532a7
MaksimVorontsov3521 <shisafanyn@gmail.com>
2026-04-25 23:05:03 +0300

    Initial commit for T2

---
+++ vorontsov.maksim/T2/Comparator.h
@@ -0,0 +1,36 @@
+#ifndef COMPARATOR_H
+#define COMPARATOR_H
+
+#include <cmath>
+
+#include "DataStruct.h"
+
+struct DataStructComparator {
+    bool operator()(const DataStruct& lhs, const DataStruct& rhs) const {
+        if (lhs.key1_ != rhs.key1_) {
+            return lhs.key1_ < rhs.key1_;
+        }
+
+        const double lhsMod = std::abs(lhs.key2_);
+        const double rhsMod = std::abs(rhs.key2_);
+        if (lhsMod != rhsMod) {
+            return lhsMod < rhsMod;
+        }
+
+        return lhs.key3_.length() < rhs.key3_.length();
+    }
+};
+
+bool compareDataStruct(const DataStruct& lhs, const DataStruct& rhs) {
+    if (lhs.key1_ != rhs.key1_) {
+        return lhs.key1_ < rhs.key1_;
+    }
+    const double lhsMag = std::abs(lhs.key2_);
+    const double rhsMag = std::abs(rhs.key2_);
+    if (lhsMag != rhsMag) {
+        return lhsMag < rhsMag;
+    }
+    return lhs.key3_.length() < rhs.key3_.length();
+}
+
+#endif
---
+++ vorontsov.maksim/T2/T2.cpp
@@ -0,0 +1,33 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "DataStruct.h"
+#include "Comparator.h"
+
+int main() {
+    std::vector<DataStruct> data{};
+    std::string line{};
+
+    while (std::getline(std::cin, line)) {
+        std::istringstream lineStream(line);
+        DataStruct item{};
+
+        if (lineStream >> item) {
+            data.push_back(item);
+        }
+    }
+
+    std::sort(data.begin(), data.end(), compareDataStruct);
+
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+
+    return 0;
+}
